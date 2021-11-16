2903e02ae59243ae11310f9f6e7e0dc20bd66e87
Marios Tsekitsidis <tseki@iastate.edu>
2021-11-16 12:11:59 -0600

    Ready to merge to main (?)

--- viszot-src/chrome/content/viszot/react-app/src/App.js
+++ viszot-src/chrome/content/viszot/react-app/src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-//import ForceGraph2D from "react-force-graph-2d";
-//import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import ForceGraph2D from "react-force-graph-2d";
+import axios from "axios";
 import './App.css';
 
 /*
@@ -9,12 +9,55 @@ const Zotero = Components.classes["@zotero.org/Zotero;1"]
                 .wrappedJSObject;
 */
 
-const App = () => {
-  return (
-    <div>
-      Hello Worldd.
+const App = () => {  // https://youtu.be/A5KaLpqzRi4
+  const [graphData, setGraphData] = useState();
+
+  const renderGraph = () => {
+    let nodesArr = [];
+    let linksArr = [];
+    const seedDOI = "10.1371/journal.ppat.1003608";
+    axios
+      .get("https://opencitations.net/index/coci/api/v1/references/"+seedDOI)
+      .then(res => {
+        console.log(res);
+        for (const dataObj of res.data) {
+          nodesArr.push({id: dataObj.cited});
+          linksArr.push({source: dataObj.citing, target: dataObj.cited, value:1});
+        }
+        nodesArr.push({id: seedDOI});  // add root node
+        setGraphData({
+          nodes: nodesArr,
+          links: linksArr
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log(nodesArr, linksArr);
+  };
+
+  useEffect(() => {
+    renderGraph()
+  }, [])
+
+  return(
+    <div className="App">
+      <div>
+        {
+          graphData ?  // https://stackoverflow.com/a/63281723
+          (
+            <ForceGraph2D
+              graphData={graphData}
+              nodeLabel="id"
+              linkCurvature="curvature"
+              enablePointerInteraction={true}
+              linkDirectionalParticleWidth={1}
+            />
+          ) : <div />
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
