dc90a319b3fcb208e850eb90c5537f88eebe0a54
Marios Tsekitsidis <tseki@iastate.edu>
2021-12-21 16:19:01 -0600

    Added basic chrome extension (with v3 manifest)

---
+++ chrome-extension/background/background.js
@@ -0,0 +1,20 @@
+// https://stackoverflow.com/q/5745822
+chrome.runtime.onInstalled.addListener(function (object) {
+    chrome.tabs.create({url: "../index.html"}, function (tab) {
+        console.log("New tab launched with index.html");
+    });
+});
+
+function getAllItems() {
+    $.ajax({
+        url:'http://127.0.0.1:23119/viszot/allItems',
+        headers: {'zotero-allowed-request':true},
+        success: function (response) {
+            $("#response").html(response);
+            console.log(response);
+        },
+        error: function (e) {
+            console.log(e);
+        },
+    });
+}
