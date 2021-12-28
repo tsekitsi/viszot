4a070168614d0c108b009ba6218b455233f32fb7
Marios Tsekitsidis <tseki@iastate.edu>
2021-12-27 21:34:51 -0600

    Added (POST) endpoint to add an item by DOI

--- zotero-plugin/src/chrome/content/viszot/init.js
+++ zotero-plugin/src/chrome/content/viszot/init.js
@@ -3,7 +3,6 @@ var Zotero = Components.classes["@zotero.org/Zotero;1"]
     .wrappedJSObject;
 
 var allItemsEndpoint = Zotero.Server.Endpoints["/viszot/allItems"] = function() {};
-
 allItemsEndpoint.prototype = {
     "supportedMethods": ["GET"],
     "init": function(postData, sendResponseCallback) {
@@ -15,5 +14,47 @@ allItemsEndpoint.prototype = {
         sendResponseCallback(200, "text/html",
             new Date() //outString //JSON.stringify(items)
         )
+    }
+}
+
+const addMyPaperEndpoint = Zotero.Server.Endpoints["/viszot/addMyPaper"] = function() {};
+// https://github.com/zotero/zotero/blob/d361930bcc4730630483625a2d7cf484f1c27554/chrome/content/zotero/lookup.js#L84
+addMyPaperEndpoint.prototype = {
+    "supportedMethods": ["POST"],
+    "supportedDataTypes": ["application/json"],
+    /**
+	 * Gets available translator list and other important data
+	 * @param {Object} postData POST data (expects doi field)
+	 * @param {Function} sendResponseCallback function to send HTTP response
+	 */
+    "init": async function(postData, sendResponseCallback) {
+        const doiID = postData.doi; //"10.3847/1538-4357/aaf9aa";
+        let newItems = false;
+        let libraryID = ZoteroPane.getSelectedLibraryID();
+		let collection = ZoteroPane.getSelectedCollection();
+        let collections = collection ? [collection.id] : false;
+        try {
+            await new Zotero.Promise(async (resolve, reject) => {
+                let translate = new Zotero.Translate.Search();
+                translate.setIdentifier({DOI: doiID});
+                let translators = await translate.getTranslators();
+                translate.setTranslator(translators);
+                try {
+                    newItems = await translate.translate({
+                        libraryID,
+                        collections,
+                        saveAttachments: true
+                    });
+                    sendResponseCallback(200, "text/html", "Added your paper!");
+                }
+                // Continue with other ids on failure
+                catch (e) {
+                    Zotero.logError(e);
+                }
+            })
+        } catch (err) {
+            Zotero.logError(err);
+        }
+        //return newItems;
     }
 }
